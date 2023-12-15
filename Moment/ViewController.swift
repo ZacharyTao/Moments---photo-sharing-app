4b6d0922ec29ea7fbc7f44f7b346c256cb13fea5
zacharytao <zacharywtao@gmail.com>
2023-12-15 17:11:48 +0800

    Initial Commit

---
+++ Moment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moment
+//
+//  Created by Zachary Tao on 12/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
